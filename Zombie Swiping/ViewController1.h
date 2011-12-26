ec63e42599535112dc384197f449d77bbe12d629
John Neumann <zombieprocessus@gmail.com>
2011-12-27 00:04:54 +0100

    Initial commit

---
+++ Zombie Swiping/ViewController1.h
@@ -0,0 +1,13 @@
+//
+//  ViewController1.h
+//  Zombie Swiping
+//
+//  Created by john on 26/12/11.
+//  Copyright (c) 2011 Zombie Processus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController1 : UIViewController
+
+@end
---
+++ Zombie Swiping/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  Zombie Swiping
+//
+//  Created by john on 26/12/11.
+//  Copyright (c) 2011 Zombie Processus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
