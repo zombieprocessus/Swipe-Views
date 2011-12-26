74801e51b44e8e612fc1bc11646e367b6da7ce1d
John Neumann <zombieprocessus@gmail.com>
2011-12-27 00:04:02 +0100

    Initial commit

--- Zombie Swiping/AppDelegate.h
+++ Zombie Swiping/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Zombie Swiping
 //
 //  Created by john on 26/12/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Zombie Pocessus. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- Zombie Swiping/AppDelegate.m
+++ Zombie Swiping/AppDelegate.m
@@ -3,7 +3,7 @@
 //  Zombie Swiping
 //
 //  Created by john on 26/12/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Zombie Processus. All rights reserved.
 //
 
 #import "AppDelegate.h"
@@ -14,10 +14,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    /*
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+     */
     return YES;
 }
 
